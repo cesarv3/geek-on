e7f782d9e1079c0ed7782883664b8b9462ccb6dc
cesar villa <cvillaf@hotmail.com>
2021-10-13 21:46:01 -0700

    Se removieron las valdiaciones crear producto

--- routes/productos.js
+++ routes/productos.js
@@ -15,15 +15,16 @@ router.get('/:id',productosController.show);
 
 //Ruta para crear un producto
 router.post('/crear', upload.single('image'), 
-body('nombre',"No puede estar vacio").notEmpty(),
-body('nombre',"Nombre debe ser mayor a 5 caracteres").isLength({ min: 5 }),
-body('price',"El precio no puede estar vacio").notEmpty(),
-body('price',"Debe ser un numero positivo").isFloat({min: 0.01}),
-body('discount',"El descuento no puede estar vacio").notEmpty(),
-body('discount',"El descuento puede estar entre 5% y 25%").isFloat({min:5, max:25}),
-body('category',"La categoria no puede estar vacia").notEmpty(),
-body('estatus',"El estatus no puede estar vacio").notEmpty(),
-body('description',"La descripcion no puede estar vacia").notEmpty(),productosController.store);
+// body('nombre',"No puede estar vacio").notEmpty(),
+// body('nombre',"Nombre debe ser mayor a 5 caracteres").isLength({ min: 5 }),
+// body('price',"El precio no puede estar vacio").notEmpty(),
+// body('price',"Debe ser un numero positivo").isFloat({min: 0.01}),
+// body('discount',"El descuento no puede estar vacio").notEmpty(),
+// body('discount',"El descuento puede estar entre 5% y 25%").isFloat({min:5, max:25}),
+// body('category',"La categoria no puede estar vacia").notEmpty(),
+// body('estatus',"El estatus no puede estar vacio").notEmpty(),
+// body('description',"La descripcion no puede estar vacia").notEmpty(),
+productosController.store);
 
 //Ruta para actualizar un producto
 router.put('/actualizar/:id',upload.single('image'),
